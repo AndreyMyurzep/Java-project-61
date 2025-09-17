536a115b09c2ed09509dab7b652f2d9103b139e2
AndreyMyurzep <hai_bk-201@mail.ru>
2025-09-17 19:41:22 +0300

    Stap 2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("Hello World!");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Brain Games!");
+    }
 }
-
